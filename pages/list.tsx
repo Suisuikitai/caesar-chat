144d97cb53af73d8f893f51739daf8d94454a552
Suisuikitai <mag.cupw@gmail.com>
2021-05-06 22:31:18 +0900

    ユーザの名前の初期表示

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,7 +1,31 @@
 import firebase from 'firebase/app'
+import React, { SyntheticEvent, useEffect, useState } from 'react'
 import Head from 'next/head'
 
 export default function list() {
+  const [name, setName] = useState('')
+  // const [active, setActive] = useState('')
+  async function listUser() {
+    const snapshot = await firebase
+      .firestore()
+      .collection('users')
+      .where('active', '==', false)
+      .get()
+
+    snapshot.docs.map((doc) => {
+      const name = doc.data().name
+      setName(name)
+    })
+  }
+
+  useEffect(() => {
+    if (!process.browser) {
+      return
+    }
+    listUser()
+    // console.log(actives)
+  }, [name])
+
   return (
     <div>
       <Head>
@@ -9,6 +33,7 @@ export default function list() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <h1>Hello World!</h1>
+      <div>{name}</div>
     </div>
   )
 }
